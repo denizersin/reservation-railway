f31adbae34fcdb25e884e3a71e32d931df2d0778
ersin <ersindenim@gmail.com>
2024-12-11 01:01:26 +0300

    test

--- src/server/api/routers/payment.ts
+++ src/server/api/routers/payment.ts
@@ -4,6 +4,7 @@ import { z } from "zod";
 import { createTRPCRouter } from "../trpc";
 import { reservationUseCases } from "@/server/layer/use-cases/reservation";
 import { clientValidator } from "@/shared/validators/front/create";
+import { env } from "@/env";
 
 
 export const paymentRouter = createTRPCRouter({
@@ -12,9 +13,14 @@ export const paymentRouter = createTRPCRouter({
 
 
         const result = await reservationUseCases.makePrepayment(opts)
-        
-    
-        return result
+
+
+        return {
+            ...result,
+            baseUrl: env.BASE_URL,
+            callbackUrl2: env.IYZIPAY_CALLBACK_URL,
+            vercelUrl: process.env.VERCEL_URL,
+        }
 
     }),
 });
